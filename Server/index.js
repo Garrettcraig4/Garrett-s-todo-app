46720324aa677feb726d9086426affb13346b78e
Garrett <Garrettcrg@gmail.com>
2018-05-06 15:33:16 -0500

    added-server-working

---
+++ Server/index.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const { json } = require("body-parser");
+
+const session = require("express-session");
+
+const cors = require("cors");
+
+const massive = require("massive");
+
+const port = process.env.port || 3001;
+
+const app = express();
+
+const { SESSION_SECRET } = process.env;
+
+app.use(json());
+
+app.use(cors());
+
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 10000000
+    }
+  })
+);
+
+app.listen(port, () => {
+  console.log(`server is on the port ${port}`);
+});
