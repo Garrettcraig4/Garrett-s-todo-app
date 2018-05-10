e1d634f4c305619cac782960720431141d1a619e
Garrett <Garrettcrg@gmail.com>
2018-05-10 15:44:59 -0500

    added-user-to-create-new-pass-and-username-to-popup

--- src/Components/Cpopup/Cpopup.js
+++ src/Components/Cpopup/Cpopup.js
@@ -5,11 +5,35 @@ import axios from "axios";
 class Cpopup extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      usern: "",
+      userp: "",
+      usere: ""
+    };
   }
 
   createuser(user, pass) {
     axios.post("/api/Createuser", { user, pass }).then(results => {
       results.data, alert("user was created go log in");
+    });
+  }
+
+  savenewusern(usern) {
+    this.setstate({
+      usern: usern
+    });
+  }
+
+  savenewuserp(userp) {
+    this.setstate({
+      userp: userp
+    });
+  }
+
+  savenewusere(usere) {
+    this.setstate({
+      usere: usere
     });
   }
 
@@ -22,12 +46,12 @@ class Cpopup extends Component {
             <h1>New Account</h1>
 
             <p>Create User Name:</p>
-            <input />
+            <input onChange={e => this.savenewusern(e.target.value)} />
             <p>Create Password:</p>
-            <input />
+            <input onChange={e => this.savenewuserp(e.target.value)} />
 
             <p>Email:</p>
-            <input />
+            <input onChange={e => this.savenewusere(e.target.value)} />
 
             <p>create account</p>
             <button>Submit</button>
