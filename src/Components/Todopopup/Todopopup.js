53be5c8222fb3514be40e26848459baf0963beae
Garrett <Garrettcrg@gmail.com>
2018-05-16 14:41:25 -0500

    added-change-title-and-button-to-popup

--- src/Components/Todopopup/Todopopup.js
+++ src/Components/Todopopup/Todopopup.js
@@ -4,12 +4,29 @@ import "./Todopopup.css";
 import axios from "axios";
 import swal from "sweetalert";
 class Todopopup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggled1: false
+    };
+    this.toggled = this.toggled.bind(this);
+  }
+  toggled() {
+    this.setState({
+      toggled1: true
+    });
+  }
+
   render() {
     return (
       <div className="Cpopup">
         <div className="box">
           <div className="stuffinbox">
-            <p>hi</p>
+            <div onClick={() => this.toggled()}>
+              {this.state.toggled1 ? <input /> : <h1>todo title</h1>}
+            </div>
+
+            <button className="plus">+ </button>
           </div>
         </div>
       </div>
