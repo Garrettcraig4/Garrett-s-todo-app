339a56d34e4ce82b09d68f369f1772167f5d7894
Garrett <Garrettcrg@gmail.com>
2018-05-16 01:33:19 -0500

    created-todopopup-needs-work

--- src/Components/Todolistmaker/Todolistmaker.js
+++ src/Components/Todolistmaker/Todolistmaker.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./Todolistmaker.css";
-
+import Todopopup from "../Todopopup/Todopopup";
 class Todolistmaker extends Component {
   constructor(props) {
     super(props);
@@ -22,12 +22,12 @@ class Todolistmaker extends Component {
     return (
       <div className="Todolistmaker">
         <h1>Add new Todo-list!</h1>
-        <button className="plus" onClick={() => clicked()}>
+        <button className="plus" onClick={() => this.clicked()}>
           {" "}
           +{" "}
         </button>
 
-        {(toggler = true ? <Todopopup /> : null)}
+        {this.state.toggler == true ? <Todopopup /> : null}
       </div>
     );
   }
---
+++ src/Components/Todopopup/Todopopup.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Todopopup.css";
+import axios from "axios";
+import swal from "sweetalert";
+class Todopopup extends Component {
+  render() {
+    return (
+      <div className="Cpopup">
+        <div className="box">
+          <div className="stuffinbox">
+            <p>hi</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Todopopup;
