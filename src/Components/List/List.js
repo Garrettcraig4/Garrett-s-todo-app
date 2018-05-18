2ad3437f35deb6e95efd8b21088dc57e9c1b1424
Garrett <Garrettcrg@gmail.com>
2018-05-18 02:16:43 -0500

    added-list-component-(needs-to-be-rendered)

---
+++ src/Components/List/List.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./List.css";
+
+class List extends Component {
+  render() {
+    return (
+      <div className="List">
+        <p>item one:</p>
+        <input />
+      </div>
+    );
+  }
+}
+
+export default List;
