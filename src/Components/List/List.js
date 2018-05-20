fdcc12a9c54c4d49dd407c2990a85d4828fce59d
Garrett <Garrettcrg@gmail.com>
2018-05-20 13:00:35 -0500

    changed-list-to-contrustor

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class List extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   render() {
     return (
       <div className="List">
