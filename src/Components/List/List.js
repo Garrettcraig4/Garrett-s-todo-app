314efde374554f94f5eba60eff862f501cb3f714
Garrett <Garrettcrg@gmail.com>
2018-05-20 19:37:19 -0500

    added-render-but-doesnt-loop-porperly

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -4,15 +4,30 @@ import { Link } from "react-router-dom";
 class List extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      userinput: 4
+    };
+    this.renders = this.renders.bind(this);
   }
+  renders(useri) {
+    for (let i = 0; i < useri; i++) {
+      return (
+        <div className="List">
+          <p>item:</p>
+          <input />
+        </div>
+      );
+    }
+  }
+
+  //loop over user amount
+
   render() {
     return (
       <div className="List">
-        <p>item one:</p>
-        <input />
-        <p>item two:</p>
-        <input />
+        {this.renders(this.state.userinput)}
+
+        <p />
       </div>
     );
   }
