def761bf7dd5ad7a87c87dc754f860425498ab8d
Garrett <Garrettcrg@gmail.com>
2018-05-23 18:15:55 -0500

    changed-home-buttons

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,22 +6,24 @@ class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <h1>Garrett's Todo List Maker!!</h1>
+        <h1>Garrett's Todo List Maker</h1>
 
         <Link to="/Creator">
-          <button className="button">Create A Todo List</button>
+          <button className="homebutton">Create A Todo List</button>
         </Link>
-        <button>Past Todo Lists</button>
 
+        <Link to="/History">
+          <button className="homebutton">Past Todo Lists</button>
+        </Link>
         <div className="des">
           <h3>Create a todo list and get those tasks done!!!! </h3>
           <p>
             With Garrett's Todo List Creator it's easy to get the things you
-            need to do complete{" "}
+            need to do complete.
           </p>
           <p>With easy methods to get you motavated </p>
           <ul>
-            <li>Colors!!</li>
+            <li>Colors!!!</li>
             <li>Messages</li>
             <li>and more </li>
           </ul>
